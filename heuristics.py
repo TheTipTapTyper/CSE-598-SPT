b2c1b3b4a171e7e39f07874a42d53ae5651ff9e6
Ryan Markson <ryanmarkson1@gmail.com>
2022-04-11 10:07:54 -0700

    modified heuristics file

--- heuristics.py
+++ heuristics.py
@@ -5,117 +5,102 @@ Created: 3/18/2022
 Class: CSE 598 Perception in Robotics
 Project: Deadlift Critic
 """
-import lm_extractor as lme
-from renderer import Renderer
+
 import numpy as np  
 from numpy.linalg import norm 
-import cv2
-from vid_man import VideoManager 
+import math
 
 
-
-
-class performance_tracker:
+def distance(x,y):
+    """Returns the absolute distance between two points. """
+    dist = norm(x-y, 2)
+    return dist
     
-    def __init__(self):
+def angle(Vec1, Vec2):
+    """Returns the angle between two vectors. """
     
- 
+    cosine_angle = np.dot(Vec1, Vec2)/(np.dot(Vec1,Vec1)*np.dot(Vec2,Vec2))
+    angle = math.degrees(math.acos(cosine_angle))
+    return angle
 
-    def knee_to_bar_dist(self, weightedAvg):
-        """Accepts the current coordinates of both knees and both hands. Returns the approximate distance 
+
+def knee_to_bar_dist(weightedAvg):
+        """Accepts the 8x3 weighted average landmark array. Returns the approximate distance 
         between the knees and the barbell (approximated by hands), taken along the axis normal to the barbell."""
-        
-        kneeAvg = weightedAvg[1,0]
-        barAvg = weightedAvg[6,0]
-        kneeToBarDist = barAvg - kneeAvg
-        # leftKnee = landmarks[25,0:2]
-        # rightKnee = landmarks[26,0:2]
-        # leftHand = landmarks[15,0:2]
-        # rightHand = landmarks[16,0:2]
-        # kneeToBarDist = max(norm(leftKnee - leftHand,2),norm(rightKnee-rightHand,2))
-        
-        
+
+        kneeAvg = weightedAvg[1, 0]
+        barAvg = weightedAvg[6, 0]
+        kneeToBarDist = distance(kneeAvg,barAvg)
         return kneeToBarDist
-        
-    def knee_to_shoulder_dist(self, landmarks):
+
+def knee_to_shoulder_dist(landmarks):
         """Accepts 33x4 numpy array of landmark coordinates, returns the approximate distance 
         between the knees and the shoulders. 
         """
-        leftKnee = landmarks[25,0:2]
-        rightKnee = landmarks[26,0:2]
-        leftShoulder = landmarks[11,0:2]
-        rightShoulder = landmarks[12,0:2]
-        kneeToShoulderDist = max(norm(leftKnee-leftShoulder,2),norm(rightKnee-rightShoulder,2))
-        
+        leftKnee = landmarks[25, 0:2]
+        rightKnee = landmarks[26, 0:2]
+        leftShoulder = landmarks[11, 0:2]
+        rightShoulder = landmarks[12, 0:2]
+        kneeToShoulderDist = max(distance(leftKnee,leftShoulder),distance(rightKnee,rightShoulder))
+
         return kneeToShoulderDist
-        
-        
-    def feet_position(self, landmarks):
+
+def feet_position(self, landmarks):
         """Accepts 33x4 numpy array of landmark coordinates; ensures that feet are roughly 
         shoulder width apart"""
         
-        distBetweenShoulders = abs(left[3,1] - right[3,1])
-        distBetweenFeet = abs(left[0,1] - right[0,1])
+        leftFoot = landmarks[27, 0:2]
+        rightFoot = landmarks[28, 0:2]
+        leftShoulder = landmarks[11, 0:2]
+        rightShoulder = landmarks[12, 0:2]
         
+
+        distBetweenShoulders = distance(leftShoulder,rightShoulder)
+        distBetweenFeet = distance(leftFoot,rightFoot)
+
         if (distBetweenShoulders*0.98 <= distBetweenFeet) and (distBetweenFeet <= distBetweenShoulders*1.02):
             return True
-        
+
         return False
-    
+
     # def barbell_position(self):
-    #     """Takes left- and right-sided landmark coordinates as input, returns the position of the barbell 
+    #     """Takes left- and right-sided landmark coordinates as input, returns the position of the barbell
     #     by approximating the barbell as the straight line which connects both of the hands"""
-        
-    
+
     # def hips_at_ground(self):
     #     """ Proper deadlift form requires that the hips are below the shoulders and above the knees at the
     #     ground (AKA rest) position. This method checks the relative positions of the three joints ,
     #     returning a pass/fail."""
-        
-    def neck_angle(self, weightedAvg):
+
+
+def neck_angle(self, weightedAvg):
         """Measures the angle made by the neck and the spine. The angle should be approximately equal to zero
         throughout the course of the lift. Returns Pass/Fail. Extract the 2D (x,y) pair which corresponds to each
         of the hip, shoulder, and ear joints. From these pairs, construct vectors which correspond to the spine
         (here, the spine is approximated as the line connecting the hips and shoulders) and the neck
         (similarly corresponding to the line which connects the shoulders to the ears). With these two lines,
         we use the relationship between the dot product of two vectors and the angle of the cosine between them."""
-        
-        spineVector = weightedAvg[3,0:2] - weightedAvg[2,0:2]
-        neckVector = weightedAvg[4,0:2] - weightedAvg[3,0:2]
-        cosine_angle = np.dot(spineVector,neckVector)/(np.dot(spineVector,spineVector)*np.dot(neckVector,neckVector))
-        degreee = math.degrees(math.acos(cosine_angle))
-        
-        if abs(degree) >=5:
+
+        spineVector = weightedAvg[3, 0:2] - weightedAvg[2,0:2]
+        neckVector = weightedAvg[4, 0:2] - weightedAvg[3,0:2]
+        degree = angle(spineVector,neckVector)
+
+        if abs(degree) >= 5:
             return False
-        
+
         return True
-        
 
+
+"""
+Revisions:
     
-    def critique_side_view(self, weightedAvg):
-        
-        #Takes 8x3 numpy array of weighted averages as input
-        
- 
+    This file should be a collection of individual methods. No classes.
+    Furthermore, decompose pre-existing methods. I.e., Decompose neck_angles
+    into two methods: one which calculates the angle between two vectors, one
+    which uses the aforementioned method to specifically return the neck angle.
 
-                        
-                if self.knee_to_bar_dist(weightedAvg):  
-                        #annotate the image accordingly
-               
-                if self.neck_angle(weightedAvg):
-                        #annotate the image accordingly
-                
-                
+"""
 
-    def critique_front_view(self, landmarks):
-        
-        #Takes 33x4 numpy array of landmark coordinates as input
-        
-
-                if self.feet_position(landmarks):
-                        #annotate the image accordingly
-                        
-     
 
                 
 

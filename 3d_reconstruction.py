dd4d5585706f597e290210e00d5c11b96851e565
Bader Alrifai <bedrrifai1@gmail.com>
2022-04-12 14:45:44 -0700

    Zhi fixed the shape issue

--- 3d_reconstruction.py
+++ 3d_reconstruction.py
@@ -19,28 +19,32 @@ def filter_2D_points(left_landmarks, right_landmarks):
     
     return np.array(left_points), np.array(right_points)
 
-# open cv _triangulate
-def _triangulate(l_proj_matrix,r_proj_matrix,l_point,r_point):
+# open cv _traginualte
+
+def helper(P):
+    points = [i[0] for i in P]
+    return [points[0]/ points[3], points[1]/points[3], points[2]/points[3]]
+
+def cv_traginualte(l_proj_matrix,r_proj_matrix,l_point,r_point):
 
     cv_triangulation = []
     for index in range(0, 33):
         triangulation = cv2.triangulatePoints(np.array(l_proj_matrix), np.array(r_proj_matrix), l_point[index], r_point[index])
-        tmp = triangulation[0: -1] / triangulation[-1]
-        cv_triangulation.append([tmp[0],tmp[1],tmp[2]])
+        cv_triangulation.append(helper(triangulation))
     return np.array(cv_triangulation)
 
 
 # direct linear transformation  
 def _DLT(P1, P2, point1, point2):
     
-    A = [
-        point1[1]*P1[2,:] - P1[1,:],
-        P1[0,:] - point1[0]*P1[2,:],
-        point2[1]*P2[2,:] - P2[1,:],
-        P2[0,:] - point2[0]*P2[2,:]
-    ]
+    A = [point1[1]*P1[2,:] - P1[1,:],
+         P1[0,:] - point1[0]*P1[2,:],
+         point2[1]*P2[2,:] - P2[1,:],
+         P2[0,:] - point2[0]*P2[2,:]
+        ]
     
     A = np.array(A).reshape((4,4))
+    # A = np.array(A).reshape((3,4))
  
     B = A.transpose() @ A
     from scipy import linalg
@@ -62,7 +66,7 @@ def two_d_to_three_d_reconstruction(l_landmark,r_landmark,left_proj_matrix, righ
 
     left_points, right_points = filter_2D_points(l_landmark, r_landmark)
 
-    cv_triangulation_points = _triangulate(left_proj_matrix, right_projection_matrix, left_points, right_points)
+    cv_triangulation_points = cv_traginualte(left_proj_matrix, right_projection_matrix, left_points, right_points)
 
     dlt_triangulation_points = dlt_triangulation(left_points, right_points, left_proj_matrix, right_proj_matrix)
 
@@ -72,8 +76,8 @@ def two_d_to_three_d_reconstruction(l_landmark,r_landmark,left_proj_matrix, righ
 # testing 
 if __name__ == "__main__":
 
-    imagesLeft = sorted(glob.glob('camera_calibration/images/poseLeft/*.png'))
-    imagesRight = sorted(glob.glob('camera_calibration/images/poseRight/*.png'))
+    imagesLeft = sorted(glob.glob('poseLeft/*.png'))
+    imagesRight = sorted(glob.glob('poseRight/*.png'))
 
     left_camera_matrix = [
         [567.81646729, 0, 489.68407262],
@@ -91,7 +95,7 @@ if __name__ == "__main__":
         [594.43441772,0,291.38330936, 0],
         [0, 594.43441772,264.21261978, 0],
         [0, 0, 1, 0]
-    ]
+ ]
     
     right_projection_matrix = [
         [5.94434418e+02, 0.00000000e+00, 2.91383309e+02, 3.84539378e+05],
@@ -105,19 +109,19 @@ if __name__ == "__main__":
     enable_segmentation=True,
     min_detection_confidence=0.5)
 
-    for left_image, right_image in zip(imagesLeft,imagesRight):
+    for left_iamge, right_iamge in zip(imagesLeft,imagesRight):
 
-        image1 = np.array(Image.open(left_image))
-        image2 = np.array(Image.open(right_image))
+        image1 = np.array(Image.open(left_iamge))
+        image2 = np.array(Image.open(right_iamge))
 
         land_marks ,  _ = lme.extract_landmarks([image1,image2],[1,2])
 
-        #return x,y,z but not visibility
+        #return x,y,z but not visiblity
         cv_triangulation_points, dlt_triangulation_points = two_d_to_three_d_reconstruction(land_marks[0],land_marks[1],left_projection_matrix,right_projection_matrix)
         
-        print("CV Shape:" , cv_triangulation_points.shape)
-        print("DLT Shape:" ,dlt_triangulation_points.shape)
-        print("CV Points:", cv_triangulation_points)
-        print("DLT Points:", dlt_triangulation_points)
+        print("CV:" , cv_triangulation_points.shape)
+        print("Dlt:" ,dlt_triangulation_points.shape)
+        #print(cv_triangulation_points)
+        #print(dlt_triangulation_points)
         break
     lme.kill_processes()

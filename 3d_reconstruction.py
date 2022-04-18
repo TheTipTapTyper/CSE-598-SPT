4fd414b866fe69c60f501ec0274ed8efc3b9afb9
Bader Alrifai <bedrrifai1@gmail.com>
2022-04-18 15:31:58 -0700

    Added new functions and cleaned code

--- 3d_reconstruction.py
+++ 3d_reconstruction.py
@@ -1,138 +1,182 @@
 import cv2
-import mediapipe as mp
+from scipy import linalg
 import glob
 import numpy as np
 import lm_extractor
 from PIL import Image
+import matplotlib.pyplot as plt
 
-
-def filter_2D_points(left_landmarks, right_landmarks):
+def filter_2D_points(l_landmarks, r_landmarks):
+    """ This function filters out the third coordinate and visibility from the landmarks.
+    It takes in two landmark arrays and then returns two arrays consisting of x and y points only 
+    """
+    
     left_points = []
-    for landmark in left_landmarks.landmark:
+    for landmark in l_landmarks.landmark:
         point = [landmark.x, landmark.y]
         left_points.append(point)
     
     right_points = []
-    for landmark in right_landmarks.landmark:
+    for landmark in r_landmarks.landmark:
         point = [landmark.x, landmark.y]
         right_points.append(point)
     
     return np.array(left_points), np.array(right_points)
 
-# open cv_triangulate
-
-def helper(P):
+def _reduce_dimension(P):
+    """ This method takes a 33x3x1 array to reduce its dimensions to 33x3
+    """
     points = [i[0] for i in P]
-    return [points[0]/ points[3], points[1]/points[3], points[2]/points[3]]
+    reduced_array = [points[0]/ points[3], points[1]/points[3], points[2]/points[3]]
+    return reduced_array
 
 def cv_triangulate(l_proj_matrix,r_proj_matrix,l_point,r_point):
-
+    """ This method uses the OpenCV triangulation method to triangulate points
+    It takes in 2 projection matrices and 2 points, and then returns the triangulated point
+    """
+    
     cv_triangulation = []
     for index in range(0, 33):
         triangulation = cv2.triangulatePoints(np.array(l_proj_matrix), np.array(r_proj_matrix), l_point[index], r_point[index])
-        cv_triangulation.append(helper(triangulation))
+        cv_triangulation.append(_reduce_dimension(triangulation))
     return np.array(cv_triangulation)
 
 
 # direct linear transformation  
-def _DLT(P1, P2, point1, point2):
-    
+def _DLT(l_proj_matrix, r_proj_matrix, l_point, r_point):
+    """ This method takes in two projection matrices and two points
+    and then uses direct linear transformation to return a triangulated point
+    """
     A = [
-        point1[1]*P1[2,:] - P1[1,:],
-        P1[0,:] - point1[0]*P1[2,:],
-        point2[1]*P2[2,:] - P2[1,:],
-        P2[0,:] - point2[0]*P2[2,:]
+        l_point[1]*l_proj_matrix[2,:] - l_proj_matrix[1,:],
+        l_proj_matrix[0,:] - l_point[0]*l_proj_matrix[2,:],
+        r_point[1]*r_proj_matrix[2,:] - r_proj_matrix[1,:],
+        r_proj_matrix[0,:] - r_point[0]*r_proj_matrix[2,:]
     ]
     
     A = np.array(A).reshape((4,4))
     B = A.transpose() @ A
-    from scipy import linalg
     U, s, Vh = linalg.svd(B, full_matrices = False)
  
-    return Vh[3,0:3]/Vh[3,3]
+    triangulated_point = Vh[3,0:3]/Vh[3,3]
+    return triangulated_point
 
 
-def dlt_triangulation(l_point, r_point, l_proj_matrix, r_proj_matrix):
-
+def dlt_triangulation(l_proj_matrix, r_proj_matrix, l_point, r_point):
+    """ This method uses the _DLT function on a series of points to triangulate all of them.
+    It takes in two projection matrices and two arrays of points, and then returns an array of triangulated points
+    """
+    
     triangulated_points = []
     for index, point in enumerate(l_point):
         triangulation = _DLT(np.array(l_proj_matrix), np.array(r_proj_matrix), point, r_point[index])
         triangulated_points.append(triangulation)
     return np.array(triangulated_points)
 
-def triangulation_with_visibility(points, l_landmark, r_landmark):
-
-    tmp , index = [], 0
-    for l, r in zip(l_landmark.landmark, r_landmark.landmark):
-        tmp.append(np.append(points[index], (l.visibility + r.visibility) /  2))
-        index += 1
-    return np.array(tmp)
-
-def two_d_to_three_d_reconstruction(l_landmark,r_landmark,left_proj_matrix, right_proj_matrix):
-
-    left_points, right_points = filter_2D_points(l_landmark, r_landmark)
-
-    cv_triangulation_points = cv_triangulate(left_proj_matrix, right_projection_matrix, left_points, right_points)
-
-    dlt_triangulation_points = dlt_triangulation(left_points, right_points, left_proj_matrix, right_proj_matrix)
-
-    cv_triangulation_points_with_visibility = triangulation_with_visibility(cv_triangulation_points,l_landmark, r_landmark)
-
-    dlt_triangulation_points_with_visibility = triangulation_with_visibility(dlt_triangulation_points,l_landmark, r_landmark)
+def add_visibility(points, l_landmark, r_landmark):
+    """ 
+    this method take 3 variable:
+        3d points  
+        left landmark
+        right landmark
     
-    # this will return 33x3 landmark np array in world cord
+    returns a 3d point with avg of the left and right landmark'visibility 
+    """
 
-    return cv_triangulation_points_with_visibility, dlt_triangulation_points_with_visibility
+    points_with_visibility  = []
+    for index, (l, r) in enumerate(zip(l_landmark.landmark, r_landmark.landmark)):
+        points_with_visibility.append(np.append(points[index], (l.visibility + r.visibility) /  2))
+    return np.array(points_with_visibility)
+
+def two_d_to_three_d_reconstruction(l_proj_matrix, r_proj_matrix, l_landmark, r_landmark):
+    """ 
+    this method take 4 variable:
+        left  projection matrix
+        right projection matrix  
+        left landmark 
+        right landmark
+
+    return a triangulate 3d point with visibility np array of size 33x4
+    """
+
+    # you only need return either cv_triangulation_points_with_visibility or dlt_triangulation_points_with_visibility
+    left_points, right_points = filter_2D_points(l_landmark, r_landmark)
+    cv_triangulation_points = cv_triangulate(l_proj_matrix, right_projection_matrix, left_points, right_points)
+    dlt_triangulation_points = dlt_triangulation(l_proj_matrix, r_proj_matrix, left_points, right_points)
+    cv_triangulation_points_with_visibility = add_visibility(cv_triangulation_points,l_landmark, r_landmark)
+    dlt_triangulation_points_with_visibility = add_visibility(dlt_triangulation_points,l_landmark, r_landmark)
+    
+    return cv_triangulation_points, dlt_triangulation_points
+
+def plot_points(points):
+    """Given a set of points, this function plots them in 3D"""
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # for bodypart, part_color in zip(body, colors):
+    #     for _c in bodypart:
+    #         ax.plot(zs = [points[_c[0],0], points[_c[1],0]], ys = [points[_c[0],1], points[_c[1],1]], xs = [points[_c[0],2], points[_c[1],2]], linewidth = 4, c = part_color)
+
+    for i in range(33):
+        ax.text(points[i,0], points[i,1], points[i,2], str(i))
+        ax.scatter(zs = points[i:i+1,0], ys = points[i:i+1,1], xs = points[i:i+1,2])
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_zticks([])
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.view_init(10, -90)
+    plt.pause(1000)
+    ax.cla()
+
+def get_projection_matrices():
+    """Gets the left and right projection matrix of the project"""
+    
+    left_projection_matrix = np.loadtxt("projMatrixL.txt",dtype=float)
+    right_projection_matrix = np.loadtxt("projMatrixR.txt", dtype=float)
+    
+    left_projection_matrix = [
+        [-9.18661764e-01, 9.11756749e+02, 3.37881846e+02, 2.01654624e+03],
+        [-9.30834700e+02, 4.13545633e+01, 2.80790957e+02, 5.90348850e+03],
+        [-8.89674392e-02, 3.33766644e-02, 9.95475159e-01, 1.47538375e+01]
+    ]
+
+    right_projection_matrix = [
+        [ 3.74945291e+01,  4.20636530e+02, 9.02796707e+02, 7.78944354e+01],
+        [-9.07387398e+02, -2.68857890e+02, 3.08500435e+02, 9.65183793e+03],
+        [ 5.51648200e-02, -6.87280340e-01, 7.24294540e-01, 2.28847151e+01]
+    ]
+
+    return left_projection_matrix, right_projection_matrix
 
 # testing 
 if __name__ == "__main__":
-
-    imagesLeft = sorted(glob.glob('poseLeft/*.png'))
-    imagesRight = sorted(glob.glob('poseRight/*.png'))
-
-    left_camera_matrix = [
-        [567.81646729, 0, 489.68407262],
-        [0, 536.034729, 294.69114695],
-        [0, 0, 1]
-    ]
-
-    right_camera_matrix = [
-        [653.48419189, 0, 343.6320513],
-        [0, 652.83410645, 248.31683002],
-        [0, 0, 1]
-    ]
+    imagesLeft = sorted(glob.glob('camera_calibration/images/poseLeft/*.png'))
+    imagesRight = sorted(glob.glob('camera_calibration/images/poseRight/*.png'))
     
-    left_projection_matrix = [
-        [594.43441772,0,291.38330936, 0],
-        [0, 594.43441772,264.21261978, 0],
-        [0, 0, 1, 0]
- ]
-    
-    right_projection_matrix = [
-        [5.94434418e+02, 0.00000000e+00, 2.91383309e+02, 3.84539378e+05],
-        [0.00000000e+00, 5.94434418e+02, 2.64212620e+02, 0.00000000e+00],
-        [0.00000000e+00, 0.00000000e+00, 1.00000000e+00, 0.00000000e+00]
-    ]
+    left_projection_matrix, right_projection_matrix = get_projection_matrices()
 
     lme = lm_extractor.LandmarkExtractor(
-    static_image_mode=True,
-    model_complexity=2,
-    enable_segmentation=True,
-    min_detection_confidence=0.5)
+        static_image_mode=True,
+        model_complexity=2,
+        enable_segmentation=True,
+        min_detection_confidence=0.5
+    )
 
     for left_image, right_image in zip(imagesLeft,imagesRight):
 
         image1 = np.array(Image.open(left_image))
         image2 = np.array(Image.open(right_image))
 
-        land_marks ,  _ = lme.extract_landmarks([image1,image2],[1,2])
+        land_marks ,  land_marks1 = lme.extract_landmarks([image1,image2],[1,2])
 
         #return x,y,z but not visibility
-        cv_triangulation_points, dlt_triangulation_points = two_d_to_three_d_reconstruction(land_marks[0],land_marks[1],left_projection_matrix,right_projection_matrix)
-        
-        print("CV:" , cv_triangulation_points.shape)
-        print("Dlt:" ,dlt_triangulation_points.shape)
-        print(cv_triangulation_points)
-        print(dlt_triangulation_points)
-        break
+        cv_triangulation_points, dlt_triangulation_points = two_d_to_three_d_reconstruction(left_projection_matrix, right_projection_matrix, land_marks[0], land_marks1[0])
+
+        plot_points(dlt_triangulation_points)
+
     lme.kill_processes()
